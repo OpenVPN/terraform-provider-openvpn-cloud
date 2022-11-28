cdd14a59d774dda8c295d79414aba25be60d0749
alexandr-korotunov-betpawa <alexandr.korotunov@betpawa.com>
2022-11-28 12:00:26 +0200

    Fix rote creation
    OpenVPN API allows only 2 parameters on route creation and fails if any of them is empty

--- client/route.go
+++ client/route.go
@@ -24,7 +24,19 @@ const (
 )
 
 func (c *Client) CreateRoute(networkId string, route Route) (*Route, error) {
-	routeJson, err := json.Marshal(route)
+
+	fmt.Println(route)
+
+	type newRoute struct {
+		Description string `json:"description"`
+		Value       string `json:"value"`
+	}
+
+	var routeToCreate newRoute
+	routeToCreate.Description = "Managed by Terraform. " + route.Description
+	routeToCreate.Value = route.Value
+
+	routeJson, err := json.Marshal(routeToCreate)
 	if err != nil {
 		return nil, err
 	}
