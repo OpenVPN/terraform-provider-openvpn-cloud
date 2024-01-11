72c62b226169db478af1c2934fd2c5e4fbbff687
Denis Arslanbekov <arslanbekov@gmail.com>
2024-01-11 00:50:35 +0000

    Refactor dns_record api

--- client/dns_record.go
+++ client/dns_record.go
@@ -15,19 +15,76 @@ type DnsRecord struct {
 	IPV6Addresses []string `json:"ipv6Addresses"`
 }
 
+type DnsRecordPageResponse struct {
+	Content          []DnsRecord `json:"content"`
+	NumberOfElements int         `json:"numberOfElements"`
+	Page             int         `json:"page"`
+	Size             int         `json:"size"`
+	Success          bool        `json:"success"`
+	TotalElements    int         `json:"totalElements"`
+	TotalPages       int         `json:"totalPages"`
+}
+
+func (c *Client) GetDnsRecordsByPage(page int, pageSize int) (DnsRecordPageResponse, error) {
+	endpoint := fmt.Sprintf("%s/api/beta/dns-records/page?page=%d&size=%d", c.BaseURL, page, pageSize)
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return DnsRecordPageResponse{}, err
+	}
+
+	body, err := c.DoRequest(req)
+	if err != nil {
+		return DnsRecordPageResponse{}, err
+	}
+
+	var response DnsRecordPageResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return DnsRecordPageResponse{}, err
+	}
+	return response, nil
+}
+
+func (c *Client) GetDnsRecord(recordId string) (*DnsRecord, error) {
+	pageSize := 10
+	page := 1
+
+	for {
+		response, err := c.GetDnsRecordsByPage(page, pageSize)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, record := range response.Content {
+			if record.Id == recordId {
+				return &record, nil
+			}
+		}
+
+		if page >= response.TotalPages {
+			break
+		}
+		page++
+	}
+	return nil, fmt.Errorf("DNS record with ID %s not found", recordId)
+}
+
 func (c *Client) CreateDnsRecord(record DnsRecord) (*DnsRecord, error) {
 	recordJson, err := json.Marshal(record)
 	if err != nil {
 		return nil, err
 	}
+
 	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/api/beta/dns-records", c.BaseURL), bytes.NewBuffer(recordJson))
 	if err != nil {
 		return nil, err
 	}
+
 	body, err := c.DoRequest(req)
 	if err != nil {
 		return nil, err
 	}
+
 	var d DnsRecord
 	err = json.Unmarshal(body, &d)
 	if err != nil {
@@ -36,37 +93,17 @@ func (c *Client) CreateDnsRecord(record DnsRecord) (*DnsRecord, error) {
 	return &d, nil
 }
 
-func (c *Client) GetDnsRecord(recordId string) (*DnsRecord, error) {
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/api/beta/dns-records", c.BaseURL), nil)
-	if err != nil {
-		return nil, err
-	}
-	body, err := c.DoRequest(req)
-	if err != nil {
-		return nil, err
-	}
-	var records []DnsRecord
-	err = json.Unmarshal(body, &records)
-	if err != nil {
-		return nil, err
-	}
-	for _, r := range records {
-		if r.Id == recordId {
-			return &r, nil
-		}
-	}
-	return nil, nil
-}
-
 func (c *Client) UpdateDnsRecord(record DnsRecord) error {
 	recordJson, err := json.Marshal(record)
 	if err != nil {
 		return err
 	}
+
 	req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("%s/api/beta/dns-records/%s", c.BaseURL, record.Id), bytes.NewBuffer(recordJson))
 	if err != nil {
 		return err
 	}
+
 	_, err = c.DoRequest(req)
 	return err
 }
@@ -76,6 +113,7 @@ func (c *Client) DeleteDnsRecord(recordId string) error {
 	if err != nil {
 		return err
 	}
+
 	_, err = c.DoRequest(req)
 	return err
 }
